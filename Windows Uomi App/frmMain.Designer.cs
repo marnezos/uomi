2847cf25175de22b962ac7309ec41dde58fe45eb
Marne <dev@marne.gr>
2017-07-04 18:20:34 +0300

    init

--- Windows Uomi App/frmMain.Designer.cs
+++ Windows Uomi App/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Windows_Uomi_App
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(599, 383);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "-Uomi-";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Windows Uomi App/frmMain.cs
+++ Windows Uomi App/frmMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Windows_Uomi_App
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
